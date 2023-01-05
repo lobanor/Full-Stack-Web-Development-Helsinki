7b1f53988c7db42aa094c91f11e7b2b21dc97f37
Lucky Obanor <obanorl@live.com>
2023-01-05 01:46:00 +0100

    05-01-2023

--- part1/anecdotes/step2/src/App.js
+++ part1/anecdotes/step2/src/App.js
@@ -11,7 +11,26 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ] // const anecdotes = [
+  ] 
+  const keys =[0,0,0,0,0,0,0]
+  
+  // var keys = [1, 2, 3];
+     
+  // An array of values
+  // var values = ["GeeksforGeeks", "Computer", "Science"];
+   
+  // Object created
+  var obj = [];
+   
+  // Using loop to insert key
+  // value in Object
+  for(var i = 0; i < keys.length; i++){
+      obj[keys[i]] = anecdotes[i];
+  }
+   
+
+
+  // const anecdotes = [
   //   {  name:  'If it hurts, do it more often.',votes:0 },
   //   {  name:  'Adding manpower to a late software project makes it later!',votes:0 },
   //   {  name:  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',votes:0 },
@@ -21,22 +40,28 @@ const App = () => {
   //   {  name:  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',votes:0 }
   // ]
  
-  const points = { 0: 0, 1: 0, 2: 0, 3: 0 }
+  // const points = { 0: 0, 1: 0, 2: 0, 3: 0 }
 
-  const copy = { ...points }
+  const copy = [...obj ]
   // increment the property 2 value by one
   copy[2] += 1     
   
-  const [first, second, third, ... rest] = anecdotes
+  // const [first, second, third, ... rest] = anecdotes
   const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(0)
   
   const nextValue = anecdotes[Math.floor(Math.random() * anecdotes.length)]
    const handleNextClick = () => {
     setSelected(nextValue)
   console.log(nextValue)}
 
+  const nextValue2 = obj[Math.floor(Math.random() * obj.length)]
+   const handleNextClick2 = () => {
+    setSelected(nextValue2)
+  console.log(nextValue2)}
+
   const handleVotesClick = () => {
-    setSelected(
+    setVote(
       copy[2]+1
     )
   console.log(nextValue)}
@@ -44,10 +69,12 @@ const App = () => {
   return (
     <div>
      <p>{selected}</p> 
+     <p>{vote}</p>
     <p><Button handleClick={handleNextClick} text='Next Anactode' />
+    <Button handleClick={handleNextClick2} text='Next obj' />
        <Button handleClick={handleVotesClick} text='Vote' />
      </p>
-    <p>{first.name} {first.votes}</p>      
+    {/* <p>{first.name} {first.votes}</p>       */}
     </div>
   )
 }
--- part1/anecdotes/step3/src/App.js
+++ part1/anecdotes/step3/src/App.js
@@ -1,53 +1,82 @@
-const Header = (props) => {
-  return (
-    <div>
-<h1>{props.course}</h1>
-      </div>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  return (
-    <div>
-<p>{props.pt1} {props.exe1}</p>
-<p>{props.pt2} {props.exe2}</p>
-<p>{props.pt3} {props.exe3}</p>
-
-      </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-<p>{props.naration}{props.total}</p>
-
-      </div>
-  )
-}
+const Button = ({ handleClick, text }) => (  <button onClick={handleClick}>{text}</button>)
 
 const App = () => {
-// const-definitions
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ] 
+  const keys =[0,0,0,0,0,0,0]
+  
+  // var keys = [1, 2, 3];
+     
+  // An array of values
+  // var values = ["GeeksforGeeks", "Computer", "Science"];
+   
+  // Object created
+  var obj = [];
+   
+  // Using loop to insert key
+  // value in Object
+  for(var i = 0; i < keys.length; i++){
+      obj[keys[i]] = anecdotes[i];
+  }
+   
 
-//
-const course = 'Half Stack application development'
-const part1 = 'Fundamentals of React '
-const exercises1 = 10
-const part2 = 'Using props to pass data '
-const exercises2 = 7
-const part3 = 'State of a component '
-const exercises3 = 14
 
-return (
-  <div>
-    <Header course={course} />
-    <Content pt1={part1}exe1={exercises1}
-        pt2={part2} exe2={exercises2}
-        pt3={part3} exe3={exercises3} />
-    <Total naration ='Number of exercises 'total={exercises1 + exercises2 + exercises3} />
+  // const anecdotes = [
+  //   {  name:  'If it hurts, do it more often.',votes:0 },
+  //   {  name:  'Adding manpower to a late software project makes it later!',votes:0 },
+  //   {  name:  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',votes:0 },
+  //   {  name:  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',votes:0 },
+  //   {  name:  'Premature optimization is the root of all evil.',votes:0 },
+  //   {  name:   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',votes:0 },
+  //   {  name:  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',votes:0 }
+  // ]
  
-  </div>
-)
+  // const points = { 0: 0, 1: 0, 2: 0, 3: 0 }
+
+  const copy = [...obj ]
+  // increment the property 2 value by one
+  copy[2] += 1     
+  
+  // const [first, second, third, ... rest] = anecdotes
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(0)
+  
+  const nextValue = anecdotes[Math.floor(Math.random() * anecdotes.length)]
+   const handleNextClick = () => {
+    setSelected(nextValue)
+  console.log(nextValue)}
+
+  const nextValue2 = obj[Math.floor(Math.random() * obj.length)]
+   const handleNextClick2 = () => {
+    setSelected(nextValue2)
+  console.log(nextValue2)}
+
+  const handleVotesClick = () => {
+    setVote(
+      copy[2]+1
+    )
+  console.log(nextValue)}
+
+  return (
+    <div>
+     <p>{selected}</p> 
+     <p>{vote}</p>
+    <p><Button handleClick={handleNextClick} text='Next Anactode' />
+    <Button handleClick={handleNextClick2} text='Next obj' />
+       <Button handleClick={handleVotesClick} text='Vote' />
+     </p>
+    {/* <p>{first.name} {first.votes}</p>       */}
+    </div>
+  )
 }
 
 export default App
